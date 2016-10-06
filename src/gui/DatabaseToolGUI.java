7e76b531d52ccd080af6ff4b9cbb68612c23d6e2
Alexander Dietrich <Alexander Dietrich>
2016-10-06 15:37:08 +0200

    Initial commit

---
+++ src/gui/DatabaseToolGUI.java
@@ -0,0 +1,10 @@
+package gui;
+
+/**
+ * Created by Alexander Dietrich on 06.10.2016.
+ */
+public class DatabaseToolGUI {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
